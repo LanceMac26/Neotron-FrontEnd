731d88ae7dabd6cdc838ba4998241edbbffe8a8a
LanceMac26 <lancemac26@gmail.com>
2022-12-06 21:36:57 +0200

    Adding in the neontron aws links

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["neotron.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost", "neotron.s3.amazonaws.com", "neotron.s3.eu-west-1.amazonaws.com"],
   },
 })
 
